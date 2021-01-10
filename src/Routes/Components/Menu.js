a17944c872815fe23e46e128d5fdfae0b557ecc7
Maria <m.skalidakh@gmail.com>
2021-01-10 16:16:36 +0200

    menu v2

--- src/Routes/Components/Menu.js
+++ src/Routes/Components/Menu.js
@@ -6,12 +6,13 @@ function Menu() {
   return (
     <nav className="nav-gen-menu">
       <ul className="nav-links">
-        <li><Link to='/'>Default<img src="" alt="" /></Link></li>
-        <li><Link to='/'>Default<img src="" alt="" /></Link></li>
-        <li><Link to='/'>Default<img src="" alt="" /></Link></li>
+        <li><Link to='/'>Home<img src="" alt="" /></Link></li>
+        <li><Link to='/'>About us<img src="" alt="" /></Link></li>
+        <li><Link to='/'>Services<img src="" alt="" /></Link></li>
+        <li><Link to='/'>Work<img src="" alt="" /></Link></li>
+        <li><Link to='/'>Contact<img src="" alt="" /></Link></li>
       </ul>
     </nav>
   );
 }
-
 export default Menu;

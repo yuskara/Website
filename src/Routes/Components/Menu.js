6258593a960fea3aff0e93f41add9759a836ca4f
Maria <m.skalidakh@gmail.com>
2021-01-11 12:29:10 +0200

    semi-responsive

--- src/Routes/Components/Menu.js
+++ src/Routes/Components/Menu.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 function Menu() {
+  const [isClicked, setisClicked] = useState(false);
   return (
     <nav className="nav-gen-menu">
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" for="menu-btn"><span className="navicon"></span></label>
       <ul className="nav-links">
-        <li><Link to='/'>Home<img src="" alt="" /></Link></li>
+        <li><Link to='/'>Home<img src="" alt=""/></Link></li>
         <li><Link to='/'>About us<img src="" alt="" /></Link></li>
         <li><Link to='/'>Services<img src="" alt="" /></Link></li>
         <li><Link to='/'>Work<img src="" alt="" /></Link></li>

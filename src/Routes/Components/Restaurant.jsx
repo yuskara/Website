9bd40c710483dc9bf45b8bcba7364847eb43fe9a
SalvadorCab <semerkhet40@hotmail.com>
2021-01-28 10:13:28 -0600

    Fixing Data

--- src/Routes/Components/Restaurant.jsx
+++ src/Routes/Components/Restaurant.jsx
@@ -9,9 +9,9 @@ import Header from "./Demo/Restaurant/Header";
 // Swith Component renders the first match and ignore the rest.//   {/* Mutaza and Presefoni Part */}
 function Restaurant() {
   return (
-    <Router>     
+    <Router>   
       <Header />
-      <Menu />
+      <Menu /> 
       <Central />
       <Footer />
     </Router>

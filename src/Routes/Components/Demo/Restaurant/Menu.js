5f6935ed78b377f644c5345966d55e40af8a80d2
Maria <m.skalidakh@gmail.com>
2021-02-01 17:14:28 +0200

    Restaurant menu fversion

--- src/Routes/Components/Demo/Restaurant/Menu.js
+++ src/Routes/Components/Demo/Restaurant/Menu.js
@@ -1,83 +1,58 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "../../../css/ResMenu.css";
 import logo_img from './imgs/rest_logo.png';
-import menu from "../../../imgs/menu.jpg";
+import { FaAlignRight } from 'react-icons/fa';
 
 function Menu() {
-  return (
-    <div className="Menu">
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_5terre.jpg">
-            <img src={menu} alt="Cinque Terre" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
+    const[navbar,setNavbar]=useState(false);
+    const [toggle, setToggle] = useState(false);
 
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
+    const changeBackground=()=>{
+        if(window.scrollY >= 100){
+            setNavbar(true)
+        }else setNavbar(false);
+    }
 
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
+    window.addEventListener('scroll',changeBackground);
+    return (
+        <>
+            <nav className= {navbar?'rest_menu active':"rest_menu"}>
+                <img src={logo_img} className="rest_logo" alt="logo"/>
+                <ul className="rest_menu-list" >
+                    <li><a href="#" className="rest_menu-link">Home</a></li>
+                    <li ><a href="#" className="rest_menu-link">About</a></li>
+                    <li><a href="#" className="rest_menu-link">Gallery</a></li>
+                    <li><a href="#" className="rest_menu-link">Menu</a>
+                        <ul className="rest_menu-dropdown">
+                            <li><a href="#">Sub-1</a></li>
+                            <li><a href="#">Sub-2</a></li>
+                            <li><a href="#">Sub-3</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#" className="rest_menu-link">Contact</a></li>
+                </ul>
+            
 
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
-
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
-
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
-
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_lights.jpg">
-            <img src={menu} alt="Northern Lights" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
-
-      <div className="responsive">
-        <div className="gallery">
-          <a target="_blank" href="img_mountains.jpg">
-            <img src={menu} alt="Mountains" width="600" height="400" />
-          </a>
-          <div className="desc">Add a description of the image here</div>
-        </div>
-      </div>
-    </div>
-  );
-}
-export default Menu;
+                <button className="rest_button-toggle"onClick={() => setToggle(!toggle)}>
+                <FaAlignRight className="rest_span-toggle"  />
+                </button>
+            </nav>
+            <div className= "rest_menu-toggle" onClick={() => setToggle(!toggle)}>
+                <ul className={toggle ?'rest_list-toggle-active':'rest_list-toggle'}>
+                    <li ><a href="#" >About</a></li>
+                    <li><a href="#" >Gallery</a></li>
+                    <li><a href="#" >Menu</a>
+                        <ul className="rest_toggle-dropdown">
+                            <li><a href="#">Sub-1</a></li>
+                            <li><a href="#">Sub-2</a></li>
+                            <li><a href="#">Sub-3</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+             </div>
+      </>
+      );
+  }
+  
+  export default Menu;

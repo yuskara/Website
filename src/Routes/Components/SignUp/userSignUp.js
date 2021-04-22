64d846a10d0b6585d968ef7412e6038501a39038
yuskara <you@example.com>
2021-04-22 06:13:10 -0400

    modified signUp files

--- src/Routes/Components/SignUp/userSignUp.js
+++ src/Routes/Components/SignUp/userSignUp.js
@@ -31,18 +31,16 @@ const UserSignUp = (props) => {
       if (name.length>=3&password.length>=3&email.length>=3){
         
                console.log('Register SUCCESS now you can Login')
-              }
-            
+              }       
     }
   }
-
 
   return(
     <div className="temp-div-signUp-container" >
       <Container>
         <Row className='justify-content-center'>
           <Col md='9' lg='7' xl='6'>
-            <Card className='mx-4-card'>
+            <Card className='temp-div-signUp-mx-4-card'>
               <CardBody className='p-4'>
                 <Form id="temp-div-signUp-form">
                   <div className="mb-2 pageheading">
@@ -50,50 +48,56 @@ const UserSignUp = (props) => {
                               Sign Up
                     </div>
                   </div>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className=' temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       onChange={handleChange}
                       name='name'
                       value={name}
-                      placeholder='Name' ></Input>
+                      placeholder='First Name' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       onChange={handleChange}
                       name='lastName'
                       value={lastName}
                       placeholder='Last Name' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className=' temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       name = 'email'
                       value = {email}
                       onChange={handleChange}
                       placeholder='email'></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='password'
                       name = 'password'
                       value = {password}
                       onChange={handleChange}
                       placeholder='password'></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='password'
                       name = 'password2'
                       value = {password2}
                       onChange={handleChange}
                       placeholder='confirm password'></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
         
                     <label for="gender">Male </label>
                     <Input
+                      className="tempForm-signup-inputRadio"
                       type='radio'
                       onChange={e =>
                         e.target.checked ? { gender: "Male"} : ""
@@ -103,6 +107,7 @@ const UserSignUp = (props) => {
                       placeholder='gender' ></Input>
                       <label for="gender">Female </label>
                       <Input
+                        className="tempForm-signup-inputRadio"
                       type='radio'
                       onChange={e =>
                         e.target.checked ? { gender: "Male"} : ""
@@ -111,41 +116,46 @@ const UserSignUp = (props) => {
                       value={gender}
                       placeholder='gender' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                   <label for="dateOfBird">Birthday </label>
                     <Input
+                      className="tempForm-signup-input"
                       type='date'
                       onChange={handleChange}
                       name='dateOfBird'
                       value={dateOfBird}
                       placeholder='date Of Bird' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
-                      <select id="country" name="country" onChange={handleChange}>
-                          <option value="country">---Country---</option>
+                  <InputGroup >
+                      <select id="country" name="country" 
+                        className='temp-div-signUp-mb-4' onChange={handleChange}>
+                          <option value={country}>---Country---</option>
                           <option value="Greece">Greece</option>
                           <option value="UK">UK</option>
                           <option value="USA">USA</option>
                       </select>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       onChange={handleChange}
                       name='language'
                       value={language}
                       placeholder='language' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       onChange={handleChange}
                       name='profession'
                       value={profession}
                       placeholder='profession' ></Input>
                   </InputGroup>
-                  <InputGroup className='mb-3'>
+                  <InputGroup className='temp-div-signUp-mb-3'>
                     <Input
+                      className="tempForm-signup-input"
                       type='text'
                       onChange={handleChange}
                       name='lookingJobAt'
@@ -165,7 +175,6 @@ const UserSignUp = (props) => {
             </Card>
           </Col>
         </Row>
-
       </Container>
     </div>
   )

066753bfe3eec0e604b041d425c3109b586edae5
salvadorcab <semerkhet40@hotmail.com>
2021-05-11 10:39:46 -0500

    Latest Changes 11/05/2021

--- src/Routes/Components/Home/Menu.js
+++ src/Routes/Components/Home/Menu.js
@@ -1,38 +1,44 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import '../../css/HomeMenu.css';
-import logo_img from '../../imgs/footer_logo.png';
+import React, { useState, useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "../../css/HomeMenu.css";
+import logo_img from "../../imgs/footer_logo.png";
+import logo_6 from "../../imgs/new.png";
+import { LanguageContext } from "./Containers/Language"; 
+import { languageOptions } from "./Languages";
 
 function Menu() {
   const [toggle, setToggle] = useState(false);
   const handleClick = () => {
     setToggle((prev) => !prev);
   };
+
   console.log(toggle);
   const navBarOpts = [
     {
-      name: 'Home',
-      link: '/',
+      name: "Home",
+      link: "/",
     },
     {
-      name: 'About us',
-      link: '/',
+      name: "Services",
+      link: "/ServicesId",
     },
     {
-      name: 'Services',
-      link: '/',
+      name: "Portfolio",
+      link: "/portfolio_more",
     },
     {
-      name: 'Our Work',
-      link: '/restaurant',
+      name: "About Us",
+      link: "/store",
     },
   ];
 
+  const { userLanguage, userLanguageChange } = useContext(LanguageContext);
+  const handleLanguageChange = (e) => userLanguageChange(e.target.value);
   const mapNavOpts = navBarOpts.map((el) => (
     <div key={el.name} onClick={() => setToggle(false)}>
-      <NavLink to={el.link} className='menu__navbar-active'>
+      <NavLink to={el.link} className="menu__navbar-active">
         {el.name}
       </NavLink>
     </div>
@@ -40,12 +46,38 @@ function Menu() {
 
   return (
     <>
-      <div className='menu_navbar-container'>
-        <img src={logo_img} alt='eskinous logo' className='menu__navbar-logo' />
-        <div className='menu__navbar-opts-container'>{mapNavOpts}</div>
+      <div className="menu_navbar-container">
+        <a href="/">
+          <img
+            src={logo_img}
+            alt="eskinous logo"
+            className="menu__navbar-logo"
+          />
+          <img
+            src={logo_6}
+            alt="eskinous logo"
+            className="menu__navbar_logo1"
+          />
+        </a>
+        <div className="menu__navbar-opts-container">{mapNavOpts}</div>
+
+        <div className="dropdown_flag">
+          <select
+            className="dropdown-content_flag"
+            onChange={handleLanguageChange}
+            value={userLanguage}
+          >
+            {Object.entries(languageOptions).map(([id, name]) => (
+              <option key={id} value={id}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+
         <FontAwesomeIcon
           icon={toggle ? faTimes : faBars}
-          className='menu__navbar-burger-menu'
+          className="menu__navbar-burger-menu"
           onClick={handleClick}
         />
       </div>
@@ -53,11 +85,11 @@ function Menu() {
       <div
         className={
           toggle
-            ? 'menu-dropdown-container'
-            : 'menu-dropdown-container-inactive'
+            ? "menu-dropdown-container"
+            : "menu-dropdown-container-inactive"
         }
       >
-        <div className='menu-dropdown-opts-cont'>{mapNavOpts}</div>
+        <div className="menu-dropdown-opts-cont">{mapNavOpts}</div>
       </div>
     </>
   );

07f80d516ebdea3099417cad19a9d40bbd5e9a07
jallow9 <jallow9@gmail.com>
2021-06-08 22:48:47 +0300

    structure of tables

--- DB/migrations/migration.js
+++ DB/migrations/migration.js
@@ -2,9 +2,66 @@ require('dotenv').config();
 
 exports.up = function(knex, Promise){
   return knex.schema
+  .createTable('userTypes', table =>{
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('fullName', 100).notNullable();
+    table.varchar('userType', 100).notNullable();
+    table.varchar('description', 100).notNullable();
+  })
+  .createTable('cvTemplates', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('cvType', 100).notNullable();
+  })
+  .createTable('languages', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('langauge', 100).notNullable();
+  })
+  .createTable('professions', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('profession', 100).notNullable();
+  })
+  .createTable('genders', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('fullName', 100).notNullable();
+    table.varchar('gender', 45).notNullable();
+  })
+  .createTable('subscriptions', table => {
+    table.increments('id');
+    table.date('subscriptionDate', 45).notNullable();
+    // table.varchar('fullName').notNullable();
+    table.varchar('subscribe', 100).notNullable();
+    table.integer('cvTemplateId').references('id').inTable('cvTemplates'); 
+  })
+  .createTable('countries', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('countryName', 100).notNullable();
+    // table.varchar('continent').notNullable();
+    // table.integer('cityId').references('id').inTable('cities');
+    // table.integer('stateId').references('id').inTable('states');
+  })
+  .createTable('states', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('state', 100).notNullable();
+    // table.integer('cityId').references('id').inTable('cities');
+    table.integer('countryId').references('id').inTable('countries');
+  })
+  .createTable('cities', table => {
+    table.increments('id');
+    table.date('creationDate', 45).notNullable();
+    table.varchar('cityName', 100).notNullable();
+    table.integer('stateId').references('id').inTable('states');
+    // table.integer('countryId').references('id').inTable('countries');
+  })
   .createTable('users', table =>{
     table.increments('id');
-    table.date('creationDate', 100).notNullable();
+    table.date('creationDate', 45).notNullable();
     table.varchar('firstName', 100).notNullable();
     table.varchar('lastName', 100).notNullable();
     table.varchar('email', 100).notNullable();
@@ -12,87 +69,29 @@ exports.up = function(knex, Promise){
     table.date('dateOfBirth', 100).notNullable();
     table.varchar('lookingJobAt', 100).notNullable();
     table.integer('userTypeId').references('id').inTable('userTypes'); // Foreign Key example
-    table.integer('countryId').references('id').inTable('countries'); 
+    // table.integer('countryId').references('id').inTable('countries'); 
     table.integer('languageId').references('id').inTable('languages'); 
     table.integer('professionId').references('id').inTable('professions'); 
     table.integer('cityId').references('id').inTable('cities'); 
-    table.integer('stateId').references('id').inTable('states'); 
+    // table.integer('stateId').references('id').inTable('states'); 
     table.integer('subscriptionId').references('id').inTable('subscriptions'); 
     table.integer('genderId').references('id').inTable('genders'); 
   })
-  .createTable('userTypes', table =>{
-    table.increments('id');
-    table.date('creationDate', 100).notNullable();
-    table.varchar('fullName', 100).notNullable();
-    table.varchar('useType', 100).notNullable();
-    table.varchar('description', 100).notNullable();
-  })
   .createTable('purchases', table =>{
     table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('fullName').notNullable();
-    table.varchar('cvType').notNullable();
-    table.float('amount').notNullable(); 
+    table.date('creationDate', 45).notNullable();
+    table.varchar('fullName', 100).notNullable();
+    table.varchar('cvType', 100).notNullable();
+    table.float('amount', 45).notNullable(); 
     table.integer('userId').references('id').inTable('users'); 
     table.integer('cvTemplateId').references('id').inTable('cvTemplates'); 
-    table.integer('countryId').references('id').inTable('countries'); 
-    table.integer('cityId').references('id').inTable('cities'); 
-    table.integer('stateId').references('id').inTable('states');
+    // table.integer('countryId').references('id').inTable('countries'); 
+    // table.integer('cityId').references('id').inTable('cities'); 
+    // table.integer('stateId').references('id').inTable('states');
     // table.integer('languageId').references('id').inTable('languages'); 
     // table.integer('professionId').references('id').inTable('professions'); 
     table.integer('intNumber').notNullable(); // 52, 12, 65
     table.float('floatNumber').notNullable(); // 12.56 , 8.90
-  })
-  .createTable('cvTemplates', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('cvType').notNullable();
-  })
-  .createTable('countries', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('countryName').notNullable();
-    table.varchar('continent').notNullable();
-    table.integer('cityId').references('id').inTable('cities');
-    table.integer('stateId').references('id').inTable('states');
-  })
-  .createTable('languages', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('langauge').notNullable();
-  })
-  .createTable('professions', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('profession').notNullable();
-  })
-  .createTable('cities', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('countryName').notNullable();
-    table.varchar('continent').notNullable();
-    table.integer('stateId').references('id').inTable('states');
-    table.integer('countryId').references('id').inTable('countries');
-  })
-  .createTable('states', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('state').notNullable();
-    table.integer('cityId').references('id').inTable('cities');
-    table.integer('countryId').references('id').inTable('countries');
-  })
-  .createTable('subscriptions', table => {
-    table.increments('id');
-    table.date('subscriptionDate').notNullable();
-    table.varchar('fullName').notNullable();
-    table.varchar('subscribe').notNullable();
-    table.integer('cvTemplateId').references('id').inTable('cvTemplates'); 
-  })
-  .createTable('genders', table => {
-    table.increments('id');
-    table.date('creationDate').notNullable();
-    table.varchar('fullName').notNullable();
-    table.varchar('gender').notNullable();
   })
 };
 
